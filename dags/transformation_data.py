b1e2996c5ec368bf5300a58bddd090dd59f45cdf
ackermanjayjay <reza23198@gmail.com>
2024-05-10 16:00:59 +0700

    fix:transformation comment

--- dags/transformation_data.py
+++ dags/transformation_data.py
@@ -2,6 +2,14 @@ from read_data import read_data
 
 
 def transformation_data():
+    """ transformation: 
+       'Beverage_category', 'Beverage', 'Beverage_prep', 'Calories',
+       ' Total Fat (g)', 'Trans Fat (g) ', 'Saturated Fat (g)', ' Sodium (mg)',
+       ' Total Carbohydrates (g) ', 'Cholesterol (mg)', ' Dietary Fibre (g)',  
+       ' Sugars (g)', ' Protein (g) ', 'Vitamin A (% DV) ', 'Vitamin C (% DV)',
+       ' Calcium (% DV) ', 'Iron (% DV) ', 'Caffeine (mg)'
+       to remove whitespace to "_" and  Capitalize to lowercase
+    """
     columns_data =  read_data()
     return columns_data.columns
 
